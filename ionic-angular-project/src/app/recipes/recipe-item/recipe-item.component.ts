58c9e0fdc2b9b568d1c274c3639ee04f1018a24b
shakeel anver <shakeelanver26@gmail.com>
2021-12-22 16:24:15 +0400

    made a separat component for recipe items

---
+++ ionic-angular-project/src/app/recipes/recipe-item/recipe-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.scss'],
+})
+export class RecipeItemComponent implements OnInit {
+  /*The @Input() makes this attribute bindable externally.
+    This is the equivalent of a react prop.
+  */
+  @Input() recipe_item: Recipe;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
